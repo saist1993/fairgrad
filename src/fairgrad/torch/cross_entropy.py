d5e8ed684e673bf68ad1a2f7fa53d3d76f5a5091
mperrot <3154060+mperrot@users.noreply.github.com>
2022-09-09 17:45:03 +0200

    add documentation for CrossEntropyLoss and FairnessLoss

--- src/fairgrad/torch/cross_entropy.py
+++ src/fairgrad/torch/cross_entropy.py
@@ -15,25 +15,11 @@ from typing import Callable, Optional, Union, Type
 
 
 class FairnessLoss(nn.modules.loss._Loss):
-    r"""This is an extension of the CrossEntropyLoss provided by pytorch. Please check pytorch documentation
-    for understanding the cross entropy loss. Here, we augment the cross entropy to enforce fairness. The
-    exact algorithm can be found in Fairgrad paper <https://arxiv.org/abs/2206.10923>.
+    r"""This is an extension of the losses provided by pytorch. Here, we augment the losses to enforce fairness. The
+    exact algorithm can be found in the Fairgrad paper <https://arxiv.org/abs/2206.10923>.
 
     Args:
-        weight (Tensor, optional): a manual rescaling weight given to each class.
-            If given, has to be a Tensor of size `C`
-        size_average (bool, optional): Deprecated (see :attr:`reduction`). By default,
-            the losses are averaged over each loss element in the batch. Note that for
-            some losses, there are multiple elements per sample. If the field :attr:`size_average`
-            is set to ``False``, the losses are instead summed for each minibatch. Ignored
-            when reduce is ``False``. Default: ``True``
-        ignore_index (int, optional): Specifies a target value that is ignored
-            and does not contribute to the input gradient. When :attr:`size_average` is
-            ``True``, the loss is averaged over non-ignored targets.
-        reduce (bool, optional): Deprecated (see :attr:`reduction`). By default, the
-            losses are averaged or summed over observations for each minibatch depending
-            on :attr:`size_average`. When :attr:`reduce` is ``False``, returns a loss per
-            batch element instead and ignores :attr:`size_average`. Default: ``True``
+        base_loss_class (nn.modules.loss._Loss): Specifies the base loss as a proper base loss.
         reduction (string, optional): Specifies the reduction to apply to the output:
             ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will
             be applied, ``'mean'``: the weighted mean of the output is taken,
@@ -41,13 +27,16 @@ class FairnessLoss(nn.modules.loss._Loss):
             and :attr:`reduce` are in the process of being deprecated, and in
             the meantime, specifying either of those two args will override
             :attr:`reduction`. Default: ``'mean'``
+        fairness_measure (string, FairnessMeasure): Currently supported are "equal_odds", "equal_opportunity", "demographic_parity", and "accuracy_parity".
         y_train (np.asarray[int], Tensor, optional): All train example's corresponding label
         s_train (np.asarray[int], Tensor, optional): All train example's corresponding sensitive attribute. This means if there
             are 2 sensitive attributes, with each of them being binary. For instance gender - (male and female) and
             age (above 45, below 45). Total unique sentive attributes are 4.
-        fairness_measure (string): Currently we support "equal_odds", "equal_opportunity", and "accuracy_parity".
+        y_desirable (np.asarray[int], Tensor, optional): All desirable labels, only used with equality of opportunity.
         epsilon (float, optional): The slack which is allowed for the final fairness level.
         fairness_rate (float, optional): Parameter which intertwines current fairness weights with sum of previous fairness rates.
+        **kwargs: Arbitrary keyword arguments passed to base_loss_class upon instantiation. Using is at your own risk as it might result in unexpected behaviours.
+
 
 
 
@@ -56,7 +45,7 @@ class FairnessLoss(nn.modules.loss._Loss):
         >>> input = torch.randn(10, 5, requires_grad=True)
         >>> target = torch.empty(10, dtype=torch.long).random_(2)
         >>> s = torch.empty(10, dtype=torch.long).random_(2) # protected attribute
-        >>> loss = CrossEntropyLoss(y_train = target, s_train = s, fairness_measure = 'equal_odds')
+        >>> loss = FairnessLoss(nn.CrossEntropyLoss,y_train = target, s_train = s, fairness_measure = 'equal_odds')
         >>> output = loss(input, target, s, mode='train')
         >>> output.backward()
     """
@@ -207,5 +196,56 @@ class FairnessLoss(nn.modules.loss._Loss):
 
 
 class CrossEntropyLoss(FairnessLoss):
-    def __init__(self, **kwargs):
-        super().__init__(nn.CrossEntropyLoss, **kwargs)
+    r"""This is an extension of the CrossEntropyLoss provided by pytorch. Please check pytorch documentation
+    for understanding the cross entropy loss.
+
+    Args:
+        reduction (string, optional): Specifies the reduction to apply to the output:
+            ``'none'`` | ``'mean'`` | ``'sum'``. ``'none'``: no reduction will
+            be applied, ``'mean'``: the weighted mean of the output is taken,
+            ``'sum'``: the output will be summed. Note: :attr:`size_average`
+            and :attr:`reduce` are in the process of being deprecated, and in
+            the meantime, specifying either of those two args will override
+            :attr:`reduction`. Default: ``'mean'``
+        fairness_measure (string, FairnessMeasure): Currently supported are "equal_odds", "equal_opportunity", "demographic_parity", and "accuracy_parity".
+        y_train (np.asarray[int], Tensor, optional): All train example's corresponding label
+        s_train (np.asarray[int], Tensor, optional): All train example's corresponding sensitive attribute. This means if there
+            are 2 sensitive attributes, with each of them being binary. For instance gender - (male and female) and
+            age (above 45, below 45). Total unique sentive attributes are 4.
+        y_desirable (np.asarray[int], Tensor, optional): All desirable labels, only used with equality of opportunity.
+        epsilon (float, optional): The slack which is allowed for the final fairness level.
+        fairness_rate (float, optional): Parameter which intertwines current fairness weights with sum of previous fairness rates.
+        **kwargs: Arbitrary keyword arguments passed to CrossEntropyLoss upon instantiation. Using is at your own risk as it might result in unexpected behaviours.
+
+    Examples::
+
+        >>> input = torch.randn(10, 5, requires_grad=True)
+        >>> target = torch.empty(10, dtype=torch.long).random_(2)
+        >>> s = torch.empty(10, dtype=torch.long).random_(2) # protected attribute
+        >>> loss = CrossEntropyLoss(y_train = target, s_train = s, fairness_measure = 'equal_odds')
+        >>> output = loss(input, target, s, mode='train')
+        >>> output.backward()
+    """
+
+    def __init__(
+        self,
+        reduction: str = "mean",
+        fairness_measure: Optional[Union[str, Type[FairnessMeasure]]] = None,
+        y_train: Optional[Union[npt.NDArray[int], Tensor]] = None,
+        s_train: Optional[Union[npt.NDArray[int], Tensor]] = None,
+        y_desirable: Optional[Union[npt.NDArray[int], Tensor]] = [1],
+        epsilon: Optional[float] = 0.0,
+        fairness_rate: Optional[float] = 0.01,
+        **kwargs,
+    ) -> None:
+        super().__init__(
+            nn.CrossEntropyLoss,
+            reduction=reduction,
+            fairness_measure=fairness_measure,
+            y_train=y_train,
+            s_train=s_train,
+            y_desirable=y_desirable,
+            epsilon=epsilon,
+            fairness_rate=fairness_rate,
+            **kwargs,
+        )
